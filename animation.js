a072c2a671d37a59e8fc52c26bc25da0d9c6eb60
remiPra <remipradere@gmail.com>
2020-03-27 23:26:27 +0100

    animation js a peu pres , opacité jeu

---
+++ animation.js
@@ -0,0 +1,20 @@
+class Animation {
+    constructor() {
+        this.presentation = document.getElementById('presentation');
+        this.skills = document.getElementById('skills');
+        this.browse = document.getElementById('browse')
+        this.presentation.addEventListener('click', this.click.bind(this))
+        this.skills.addEventListener('click', this.click.bind(this))
+
+    }
+    click() {
+
+        this.browse.classList.add("animation");
+        console.log('succes');
+        setTimeout(function() { this.browse.classList.remove("animation"); }, 3000);
+
+
+    }
+}
+
+let start = new Animation();
